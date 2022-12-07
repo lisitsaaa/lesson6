783153231b4d89e398a821a62c2d9fd9b4128ec0
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-12-07 23:13:38 +0300

    task about atm

--- src/ATM/Atm.java
+++ src/ATM/Atm.java
@@ -5,9 +5,9 @@ public class Atm {
     int banknotes50;
     int banknotes100;
 
-    int col20;
-    int col50;
-    int col100;
+    int col20 = 0;
+    int col50 = 0;
+    int col100 = 0;
     int sum;
 
     Atm(int banknotes20, int banknotes50, int banknotes100) {
@@ -18,7 +18,7 @@ public class Atm {
     }
 
     public void addMoney(int banknotes20, int banknotes50, int banknotes100) {
-        sum = sum + banknotes20 * 20 + banknotes50 * 50 + banknotes100 * 100;
+        this.sum = sum + banknotes20 * 20 + banknotes50 * 50 + banknotes100 * 100;
         this.banknotes20 += banknotes20;
         this.banknotes50 += banknotes50;
         this.banknotes100 += banknotes100;
@@ -32,33 +32,47 @@ public class Atm {
         }
 
         boolean status = false;
-        int intermediateSum;
-        int countSum = 1000;
+        int tmpSum = amount;
 
-        for (int i = 0; i < this.banknotes20; i++) {
-            for (int j = 0; j < this.banknotes50; j++) {
-                for (int k = 0; k < this.banknotes100; k++) {
-                    intermediateSum = i * 20 + j * 50 + k * 100;
-                    if (intermediateSum == amount) {
-                        if (countSum > i + j + k) {
-                            countSum = i + j + k;
-                            this.col20 = i;
-                            this.col50 = j;
-                            this.col100 = k;
-                            sum = sum - i * 20 - j * 50 - k * 100;
-                        }
-                        status = true;
-                    }
-                }
+        while (!status) {
+            if (((amount - 100) % 20) == 0 && ((amount - 100) % 50) == 0) {
+                col100++;
+
+            } else if (((amount - 50) % 20) == 0) {
+                col50++;
+
+            } else if (amount % 20 == 0) {
+                col20++;
+            }
+            if (amount == col100 * 100 + col50 * 50 + col20 * 20) {
+                status = true;
             }
         }
 
+//        for (int i = 0; i < this.banknotes20; i++) {
+//            for (int j = 0; j < this.banknotes50; j++) {
+//                for (int k = 0; k < this.banknotes100; k++) {
+//                    intermediateSum = i * 20 + j * 50 + k * 100;
+//                    if (intermediateSum == amount) {
+//                        if (countSum > i + j + k) {
+//                            countSum = i + j + k;
+//                            this.col20 = i;
+//                            this.col50 = j;
+//                            this.col100 = k;
+//                            sum = sum - i * 20 - j * 50 - k * 100;
+//                        }
+//                        status = true;
+//                    }
+//                }
+//            }
+//        }
+//
         if (status) {
             this.banknotes20 -= col20;
             this.banknotes50 -= col50;
             this.banknotes100 -= col100;
         }
-        //sum = sum - this.banknotes20 - this.banknotes50 - this.banknotes100;
+
         System.out.println(status);
         return status;
     }
